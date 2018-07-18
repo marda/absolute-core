9c81d510804bf733cb01280ea61c942f63f3dbc5
Jiri Marousek <jirkamarda@seznam.cz>
2018-07-18 21:30:32 +0200

    a
    a

---
+++ manager/BaseCRUDManager.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Absolute\Core\Manager;
+
+use Nette\Database\Context;
+
+class BaseCRUDManager
+{
+  /** @var Nette\Database\Context */
+  protected $database;
+
+  public function __construct(Context $database)
+  {
+    $this->database = $database;
+  }  
+}
---
+++ presenters/BaseRestPresenter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Absolute\Core\Presenter;
+
+use Absolute\Core\Response\JsonResponse;
+
+class BaseRestPresenter extends BasePresenter
+{
+  /** @var property */
+  protected $jsonResponse;
+
+  public function startup() 
+  {
+    parent::startup();
+
+    $this->jsonResponse = new JsonResponse;
+  }
+
+  public function getJsonResponse()
+  {
+    return $this->jsonResponse;
+  }
+}
